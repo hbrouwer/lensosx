6f16d9294de1a2a80544902470bb76bc359c1da7
Harm Brouwer <me@hbrouwer.eu>
2011-10-07 14:27:28 +0200

    beautified a bit

--- src/shell.tcl
+++ src/shell.tcl
@@ -489,7 +489,7 @@ proc .showAbout {} {
 	-fg \#888888 -font .LargeFont 
     label $w.top.version -text "version ${.Version}\nwritten by Doug Rohde (dr+lens@cs.cmu.edu)\nCenter for the Neural Basis of Cognition\nCarnegie Mellon University" -font .LargeFont -justify center
 
-    label $w.top.osxport -text "MacOSX port by Harm Brouwer' (me@hbrouwer.eu),\nDaniel de Kok' (me@danieldk.eu) and Hartmut Fitz* (hartmut.fitz@gmail.com)\n' University of Groningen, Center for Language and Cognition\n* Max Planck Institute for Psycholinguistics\n\nBrain in vat icon by Cian Walsh." -font .LargeFont -justify center
+    label $w.top.osxport -text "MacOSX port by Harm Brouwer' (me@hbrouwer.eu),\nDaniel de Kok' (me@danieldk.eu) and Hartmut Fitz* (hartmut.fitz@gmail.com)\n' University of Groningen, Center for Language and Cognition\n* Max Planck Institute for Psycholinguistics\n\nBrain in vat icon by Cian Walsh\n" -font .LargeFont -justify center
 
     label $w.top.message -text "Lens and its source code is available free of charge only for experimental research at academic institutions.  A license must be obtained from Doug Rohde if Lens is to be used by other individuals or for other purposes.\n\nLens was originally based on the Xerion neural network simulator produced by Drew van Camp and Tony Plate at the University of Toronto.  The design of the graphical displays and the implementation of the training algorithms borrows somewhat from Xerion V3.1.\n\nThe graphical and scripting interfaces to Lens were written using the Tcl/Tk Toolkit, available at http://www.scriptics.com." \
 	-justify left -wraplength 500
